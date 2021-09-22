3f4bea5a43d2be93d6ad5afd7980b88cb197960d
Jan Weiß <jan@geheimwerk.de>
2021-09-22 13:04:08 +0200

    Cleaned indentation.

--- src/main.c
+++ src/main.c
@@ -26,33 +26,33 @@ extern MQTTClient client;
 
 /* signal handler for ^C: closing all files, windows, mutexes, etc. */
 void sig_handler(int signum) {
-    if(signum != SIGINT) {
+	if(signum != SIGINT) {
 		printf("Invalid signum %d\n", signum);
 	}
 
-    pthread_cancel(pcm_sampling_hndl);
-    pthread_cancel(vad_hndl);
-    pthread_cancel(mqtt_hndl);
+	pthread_cancel(pcm_sampling_hndl);
+	pthread_cancel(vad_hndl);
+	pthread_cancel(mqtt_hndl);
 
-    mq_unlink(TOPIC);
+	mq_unlink(TOPIC);
 
-    pthread_mutex_destroy(&mx_sync1);
-    pthread_mutex_destroy(&mx_sync2);
-    sem_destroy(&sx_vadLock1);
-    sem_destroy(&sx_vadLock2);
+	pthread_mutex_destroy(&mx_sync1);
+	pthread_mutex_destroy(&mx_sync2);
+	sem_destroy(&sx_vadLock1);
+	sem_destroy(&sx_vadLock2);
 
 #ifdef FILES
-    fclose(fFeatures);
-    fclose(fSignal);
-    fclose(fSpectrum);
-    fclose(fVad);
+	fclose(fFeatures);
+	fclose(fSignal);
+	fclose(fSpectrum);
+	fclose(fVad);
 #endif
 
 #ifdef MQTT
 	MQTTClient_disconnect(client, 10000);
 	MQTTClient_destroy(&client);
-  mq_close(mq_read);
-  mq_close(mq_write);
+	mq_close(mq_read);
+	mq_close(mq_write);
 #endif
 
 	printf("\nBye\n");
@@ -60,32 +60,32 @@ void sig_handler(int signum) {
 }
 
 int main() {
-  printf("pzar 2019\n");
-  signal(SIGINT, sig_handler);
-  sem_init(&sx_vadLock1, 1, 0);
-  sem_init(&sx_vadLock2, 1, 1);
-  if (pthread_mutex_init(&mx_sync2, NULL) != 0) {
-      printf("\nMutex init failed!\n");
-      return 0;
-  }
-  if (pthread_mutex_init(&mx_sync1, NULL) != 0) {
-      printf("\nMutex init failed!\n");
-      return 0;
-  }
+	printf("pzar 2019\n");
+	signal(SIGINT, sig_handler);
+	sem_init(&sx_vadLock1, 1, 0);
+	sem_init(&sx_vadLock2, 1, 1);
+	if (pthread_mutex_init(&mx_sync2, NULL) != 0) {
+			printf("\nMutex init failed!\n");
+			return 0;
+	}
+	if (pthread_mutex_init(&mx_sync1, NULL) != 0) {
+			printf("\nMutex init failed!\n");
+			return 0;
+	}
 
-  printf("Threads starting\n");
-  pthread_create(&pcm_sampling_hndl, NULL, &pcm_sampling_thrd, NULL);
-  pthread_create(&vad_hndl, NULL, &vad_thrd, NULL);
-  #ifdef MQTT
-  pthread_create(&mqtt_hndl, NULL, &mqtt_pub_thrd, NULL);
-  #endif
+	printf("Threads starting\n");
+	pthread_create(&pcm_sampling_hndl, NULL, &pcm_sampling_thrd, NULL);
+	pthread_create(&vad_hndl, NULL, &vad_thrd, NULL);
+	#ifdef MQTT
+	pthread_create(&mqtt_hndl, NULL, &mqtt_pub_thrd, NULL);
+	#endif
 
-  printf("Wait for join\n");
+	printf("Wait for join\n");
 	while (1);
 
-  pthread_join(pcm_sampling_hndl, NULL);
-  pthread_join(vad_hndl, NULL);
-  pthread_join(mqtt_hndl, NULL);
+	pthread_join(pcm_sampling_hndl, NULL);
+	pthread_join(vad_hndl, NULL);
+	pthread_join(mqtt_hndl, NULL);
 
-  return 0;
+	return 0;
 }
--- src/pcm_capture.c
+++ src/pcm_capture.c
@@ -9,13 +9,13 @@ void* pcm_sampling_thrd() {
 	snd_pcm_hw_params_t *pcm_params;
 	snd_pcm_uframes_t pcm_frames;
 
-	sampling_rate = SAMPLING_RATE;						    // set sampling rate
-	pcm_frames = FFT_POINTS;					            // set number of frames
-	real_buffer = (short *) malloc((int)pcm_frames * 2);    // frames * short size
+	sampling_rate = SAMPLING_RATE;							 // set sampling rate
+	pcm_frames = FFT_POINTS;								  // set number of frames
+	real_buffer = (short *) malloc((int)pcm_frames * 2);	 // frames * short size
 
 	// rc = snd_pcm_open(&pcm_hndl, "default", SND_PCM_STREAM_CAPTURE, 0);
-      /* in case of problem with hardware use this one "$ arecord -l" */
-      rc = snd_pcm_open(&pcm_hndl, "hw:3,0", SND_PCM_STREAM_CAPTURE, 0);
+	  /* in case of problem with hardware use this one "$ arecord -l" */
+	  rc = snd_pcm_open(&pcm_hndl, "hw:3,0", SND_PCM_STREAM_CAPTURE, 0);
 	if (rc < 0) {
 		fprintf(stderr, "Unable to open pcm device: %s\n", snd_strerror(rc));
 		exit(1);
@@ -28,33 +28,33 @@ void* pcm_sampling_thrd() {
 	snd_pcm_hw_params_set_channels(pcm_hndl, pcm_params, 1);
 	snd_pcm_hw_params_set_rate_near(pcm_hndl, pcm_params, &sampling_rate, &dir);
 	if(SAMPLING_RATE != sampling_rate) {
-	    fprintf(stderr, "The rate %d Hz is not supported, using %d Hz instead\n", SAMPLING_RATE, sampling_rate);
+		 fprintf(stderr, "The rate %d Hz is not supported, using %d Hz instead\n", SAMPLING_RATE, sampling_rate);
 	}
 	snd_pcm_hw_params_set_period_size_near(pcm_hndl, pcm_params, &pcm_frames, &dir);
 	rc = snd_pcm_hw_params(pcm_hndl, pcm_params);
 	if (rc < 0) {
-	    fprintf(stderr, "unable to set hw parameters: %s \n", snd_strerror(rc));
-	    exit(1);
+		 fprintf(stderr, "unable to set hw parameters: %s \n", snd_strerror(rc));
+		 exit(1);
 	}
 
 	/* Main LOOP for data acquisition */
 	while(1) {
-        // pthread_mutex_lock(&mx_sync1);
-        sem_wait(&sx_vadLock2);            // wait for semaphore
-        rc = snd_pcm_readi(pcm_hndl, real_buffer, pcm_frames);
-        if (rc == -EPIPE) {
-            fprintf(stderr, "Overrun\n");
-            snd_pcm_prepare(pcm_hndl);
-        }
-        else if (rc < 0) {
-            fprintf(stderr, "Error while reading: %s\n", snd_strerror(rc));
-        }
-        else if (rc == pcm_frames){
-          // pthread_mutex_unlock(&mx_sync1);
-          sem_post(&sx_vadLock1);
-        }
-        else {
+		// pthread_mutex_lock(&mx_sync1);
+		sem_wait(&sx_vadLock2);			   // wait for semaphore
+		rc = snd_pcm_readi(pcm_hndl, real_buffer, pcm_frames);
+		if (rc == -EPIPE) {
+			fprintf(stderr, "Overrun\n");
+			snd_pcm_prepare(pcm_hndl);
+		}
+		else if (rc < 0) {
+			fprintf(stderr, "Error while reading: %s\n", snd_strerror(rc));
+		}
+		else if (rc == pcm_frames){
+		  // pthread_mutex_unlock(&mx_sync1);
+		  sem_post(&sx_vadLock1);
+		}
+		else {
 
-        }
+		}
 	}
 }
